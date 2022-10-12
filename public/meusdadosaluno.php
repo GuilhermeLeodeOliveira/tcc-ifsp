d6c27fc9d468b117ee468f96ec45fa8e84bd87f3
GuilhermeLeodeOliveira <97077427+GuilhermeLeodeOliveira@users.noreply.github.com>
2022-10-12 17:26:03 -0300

    Update meusdadosaluno.php

--- public/meusdadosaluno.php
+++ public/meusdadosaluno.php
@@ -3,17 +3,6 @@
   session_start();
   require_once("conect.php");
 
-  function Mask($mask,$str){
-
-    $str = str_replace(" ","",$str);
-
-    for($i=0;$i<strlen($str);$i++){
-        $mask[strpos($mask,"#")] = $str[$i];
-    }
-
-    return $mask;
-
-}
 
 ?>
 
